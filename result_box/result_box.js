64583c77a2db0cdbc83e3a22d21d4b579bdc8ebd
fabiovalse <fabio.valsecchi@iit.cnr.it>
2015-03-05 19:30:39 +0100

    Search event and triggering

--- result_box/result_box.js
+++ result_box/result_box.js
@@ -1,8 +1,14 @@
 (function() {
 window.result_box = {};
 
-result_box.init = function() {
-    console.log('ok result');
+result_box.init = function(dom_selector) {
+    box = d3.select(dom_selector);
+
+    result_box.node = box;
+}
+
+result_box.update = function(selection) {
+	console.log(selection);
 }
 
 }).call(this);
