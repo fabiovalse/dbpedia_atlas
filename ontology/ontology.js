04e537a78e0d9e3f8b764e0837896e3a2a66befa
nitaku <matteo.abrate@gmail.com>
2015-03-06 21:16:21 +0100

    New ontology method to get a node from the corresponding class.

--- ontology/ontology.js
+++ ontology/ontology.js
@@ -1,8 +1,18 @@
 (function() {
 window.ontology = {}
 
+var _index = {}
+_create_index = function(n) {
+    _index[n.name] = n;
+    n.children.forEach(function(c){
+        _create_index(c);
+    });
+}
+
 ontology.init = function(data) {
     ontology.tree = data;
+    
+    _create_index(ontology.tree);
 }
 
 // Returns the most specific class
@@ -17,6 +27,11 @@ ontology.get_msc = function(node, classes) {
     });
 
     return classes;
+}
+
+// Returns the tree node corresponding to the given class
+ontology.get_node_from_class = function(klass) {
+    return _index[klass];
 }
 
 }).call(this);
