1decbc8d89c89c37f62aeb884da1eec9c2f83267
fabiovalse <fabio.valsecchi@iit.cnr.it>
2015-03-06 10:56:20 +0100

    Result Box creation and click

--- result_box/result_box.js
+++ result_box/result_box.js
@@ -8,7 +8,26 @@ result_box.init = function(dom_selector) {
 }
 
 result_box.update = function(selection) {
-	console.log(selection);
+	result_box.node.html("");
+	result_box.node.style("display", "inline");
+
+	/*html = "";
+	selection.forEach(function(d) { html += "<div class='entity_result' title='" + d.uri + "'>" + format_uri(d.uri) + "</div>";});
+	result_box.node.html(html);*/
+
+	var results = result_box.node.selectAll('.entity_result')
+		.data(selection);
+
+	results.enter().append('div')
+		.attr('class', 'entity_result')
+		.attr('id', function(d) {return d.uri;})
+		.text(function(d) {return format_uri(d.uri);})
+		.on('click', function() {
+			trigger(result_box.node, 'select', {uri: this.id});
+
+			result_box.node.style("display", "none");
+			search_box.node.text("");
+		});
 }
 
 }).call(this);
--- utils.js
+++ utils.js
@@ -10,3 +10,31 @@ function trigger(target, name, extra) {
     e.extra = extra;
     target.dispatchEvent(e);
 }
+
+var format_uri = function(key) {
+	var newKey = "";
+	var flag = false;
+
+	key = decodeURIComponent(key).replace(/_/g, " ");
+	uriKey = key;
+
+	splittedKey = key.split("/");
+	key = splittedKey[splittedKey.length-1];
+
+	if (key.indexOf("#") != -1) {
+		key = key.substring(key.indexOf("#")+1);
+	}
+
+	for (i in key) {
+		if (!flag && key.charAt(i) === key.charAt(i).toUpperCase() && key.charAt(i) !== key.charAt(i).toLowerCase()) {
+			newKey += " ";
+			newKey += key.charAt(i);
+			flag = true;
+		} else {
+			newKey += key.charAt(i);
+			flag = false;
+		}
+	}
+
+	return (newKey.charAt(0).toUpperCase() + newKey.slice(1));
+}
