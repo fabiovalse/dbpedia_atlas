71cbadd598cec1471a7a61d3546b7f63f370212b
fabiovalse <fabio.valsecchi@iit.cnr.it>
2015-03-09 14:51:12 +0100

    classes addition in get_entity API

--- api/get_entity.php
+++ api/get_entity.php
@@ -13,7 +13,8 @@
 
 		$uri_query = "<" . urlencode("http://dbpedia.org/resource/") . $entity . ">";
 
-		$query = urlencode("SELECT ?s ?p ?o ?i ?j {{?g ?p ?o .OPTIONAL {?o <http://wafi.iit.cnr.it/lod/ns/atlas#i> ?i.?o <http://wafi.iit.cnr.it/lod/ns/atlas#j> ?j.}FILTER (?g = ") . $uri_query . urlencode(" && ?p != <http://www.w3.org/2002/07/owl#sameAs>)BIND (?g AS ?s)}UNION{?s ?p ?g .OPTIONAL {?s <http://wafi.iit.cnr.it/lod/ns/atlas#i> ?i.?s <http://wafi.iit.cnr.it/lod/ns/atlas#j> ?j.}FILTER (?g = ") . $uri_query . urlencode(" && ?p != <http://www.w3.org/2002/07/owl#sameAs>)BIND (?g AS ?o)}}");
+		#$query = urlencode("SELECT ?s ?p ?o ?i ?j {{?g ?p ?o .OPTIONAL {?o <http://wafi.iit.cnr.it/lod/ns/atlas#i> ?i.?o <http://wafi.iit.cnr.it/lod/ns/atlas#j> ?j.}FILTER (?g = ") . $uri_query . urlencode(" && ?p != <http://www.w3.org/2002/07/owl#sameAs>)BIND (?g AS ?s)}UNION{?s ?p ?g .OPTIONAL {?s <http://wafi.iit.cnr.it/lod/ns/atlas#i> ?i.?s <http://wafi.iit.cnr.it/lod/ns/atlas#j> ?j.}FILTER (?g = ") . $uri_query . urlencode(" && ?p != <http://www.w3.org/2002/07/owl#sameAs>)BIND (?g AS ?o)}}");
+		$query = urlencode("SELECT ?s ?p ?o ?c ?i ?j {{?g ?p ?o .OPTIONAL {?o <http://wafi.iit.cnr.it/lod/ns/atlas#i> ?i.?o <http://wafi.iit.cnr.it/lod/ns/atlas#j> ?j.?o a ?c.FILTER (STRSTARTS(STR(?c), 'http://dbpedia.org/ontology/') && !STRSTARTS(STR(?c), 'http://dbpedia.org/ontology/Wikidata'))}FILTER (?g = ") . $uri_query . urlencode(" && ?p != <http://www.w3.org/2002/07/owl#sameAs>)BIND (?g AS ?s)}UNION{?s ?p ?g .OPTIONAL {?s <http://wafi.iit.cnr.it/lod/ns/atlas#i> ?i.?s <http://wafi.iit.cnr.it/lod/ns/atlas#j> ?j.?s a ?c.FILTER (STRSTARTS(STR(?c), 'http://dbpedia.org/ontology/') && !STRSTARTS(STR(?c), 'http://dbpedia.org/ontology/Wikidata'))}FILTER (?g = ") . $uri_query . urlencode(" && ?p != <http://www.w3.org/2002/07/owl#sameAs>)BIND (?g AS ?o)}}");
 
 		$query = str_replace("%3C", "<", $query);
 		$query = str_replace("%3E", ">", $query);
@@ -26,13 +27,15 @@
 		$i = $_GET["i"];
 		$j = $_GET["j"];
 
-		$query = urlencode('SELECT ?s ?p ?o ?i ?j {{?g <http://wafi.iit.cnr.it/lod/ns/atlas#i> "' . $i . '"^^<http://www.w3.org/2001/XMLSchema#integer>;<http://wafi.iit.cnr.it/lod/ns/atlas#j> "' . $j . '"^^<http://www.w3.org/2001/XMLSchema#integer>;?p ?o .OPTIONAL {?o <http://wafi.iit.cnr.it/lod/ns/atlas#i> ?i.?o <http://wafi.iit.cnr.it/lod/ns/atlas#j> ?j.}FILTER (?p != <http://www.w3.org/2002/07/owl#sameAs>)BIND (?g AS ?s)}UNION{?g <http://wafi.iit.cnr.it/lod/ns/atlas#i> "' . $i . '"^^<http://www.w3.org/2001/XMLSchema#integer>;<http://wafi.iit.cnr.it/lod/ns/atlas#j> "' . $j . '"^^<http://www.w3.org/2001/XMLSchema#integer>.?s ?p ?g .OPTIONAL {?s <http://wafi.iit.cnr.it/lod/ns/atlas#i> ?i.?s <http://wafi.iit.cnr.it/lod/ns/atlas#j> ?j.}FILTER (?p != <http://www.w3.org/2002/07/owl#sameAs>)BIND (?g AS ?o)}}');
+		#$query = urlencode('SELECT ?s ?p ?o ?i ?j {{?g <http://wafi.iit.cnr.it/lod/ns/atlas#i> "' . $i . '"^^<http://www.w3.org/2001/XMLSchema#integer>;<http://wafi.iit.cnr.it/lod/ns/atlas#j> "' . $j . '"^^<http://www.w3.org/2001/XMLSchema#integer>;?p ?o .OPTIONAL {?o <http://wafi.iit.cnr.it/lod/ns/atlas#i> ?i.?o <http://wafi.iit.cnr.it/lod/ns/atlas#j> ?j.}FILTER (?p != <http://www.w3.org/2002/07/owl#sameAs>)BIND (?g AS ?s)}UNION{?g <http://wafi.iit.cnr.it/lod/ns/atlas#i> "' . $i . '"^^<http://www.w3.org/2001/XMLSchema#integer>;<http://wafi.iit.cnr.it/lod/ns/atlas#j> "' . $j . '"^^<http://www.w3.org/2001/XMLSchema#integer>.?s ?p ?g .OPTIONAL {?s <http://wafi.iit.cnr.it/lod/ns/atlas#i> ?i.?s <http://wafi.iit.cnr.it/lod/ns/atlas#j> ?j.}FILTER (?p != <http://www.w3.org/2002/07/owl#sameAs>)BIND (?g AS ?o)}}');
+		$query = urlencode('SELECT ?s ?p ?o ?c ?i ?j {{?g <http://wafi.iit.cnr.it/lod/ns/atlas#i> "' . $i . '"^^<http://www.w3.org/2001/XMLSchema#integer>;<http://wafi.iit.cnr.it/lod/ns/atlas#j> "' . $j . '"^^<http://www.w3.org/2001/XMLSchema#integer>;?p ?o .OPTIONAL {?o <http://wafi.iit.cnr.it/lod/ns/atlas#i> ?i.?o <http://wafi.iit.cnr.it/lod/ns/atlas#j> ?j.?o a ?c.FILTER (STRSTARTS(STR(?c), "http://dbpedia.org/ontology/") && !STRSTARTS(STR(?c), "http://dbpedia.org/ontology/Wikidata"))}FILTER (?p != <http://www.w3.org/2002/07/owl#sameAs>)BIND (?g AS ?s)}UNION{?g <http://wafi.iit.cnr.it/lod/ns/atlas#i> "' . $i . '"^^<http://www.w3.org/2001/XMLSchema#integer>;<http://wafi.iit.cnr.it/lod/ns/atlas#j> "' . $j . '"^^<http://www.w3.org/2001/XMLSchema#integer>.?s ?p ?g .OPTIONAL {?s <http://wafi.iit.cnr.it/lod/ns/atlas#i> ?i.?s <http://wafi.iit.cnr.it/lod/ns/atlas#j> ?j.?s a ?c.FILTER (STRSTARTS(STR(?c), "http://dbpedia.org/ontology/") && !STRSTARTS(STR(?c), "http://dbpedia.org/ontology/Wikidata"))}FILTER (?p != <http://www.w3.org/2002/07/owl#sameAs>)BIND (?g AS ?o)}}');
 		$result = make_query($query);
 
 		$uri = $result["results"]["bindings"][0]["s"]["value"];
 	}
 
 	$data = array("uri" => $uri, "data_properties" => array(), "object_properties" => array("incoming" => array(), "outgoing" => array(), "itself" => array()), "types" => array());
+	$current_entity = "";
 
 	foreach ($result["results"]["bindings"] as $triple) {
 		# object_properties -> itself
@@ -40,17 +43,30 @@
 			array_push($data["object_properties"]["itself"], $triple["p"]);
 		# object_properties -> outgoing
 		else if ($triple["s"]["value"] == $uri && strpos($triple["o"]["value"], "http://dbpedia.org/resource/") > -1) {
-			if (array_key_exists("i", $triple))
-				array_push($data["object_properties"]["outgoing"], array("p" => $triple["p"], "o" => $triple["o"], "i" => $triple["i"], "j" => $triple["j"]));
-			else
-				array_push($data["object_properties"]["outgoing"], array("p" => $triple["p"], "o" => $triple["o"]));
+			if ($current_entity != $triple["o"]["value"]) {
+				$current_entity = $triple["o"]["value"];
+
+				if (array_key_exists("i", $triple))
+					array_push($data["object_properties"]["outgoing"], array("p" => $triple["p"], "o" => $triple["o"], "c" => array($triple["c"]), "i" => $triple["i"], "j" => $triple["j"]));
+				else
+					array_push($data["object_properties"]["outgoing"], array("p" => $triple["p"], "o" => $triple["o"]));
+			} else {
+				array_push($data["object_properties"]["outgoing"][count($data["object_properties"]["outgoing"])-1]["c"], $triple["c"]);
+			}
 		}
 		# object_properties -> incoming
-		elseif ($triple["o"]["value"] == $uri && strpos($triple["s"]["value"], "http://dbpedia.org/resource/") > -1)
-			if (array_key_exists("i", $triple))
-				array_push($data["object_properties"]["incoming"], array("s" => $triple["s"], "p" => $triple["p"], "i" => $triple["i"], "j" => $triple["j"]));
-			else	
-				array_push($data["object_properties"]["incoming"], array("s" => $triple["s"], "p" => $triple["p"]));
+		elseif ($triple["o"]["value"] == $uri && strpos($triple["s"]["value"], "http://dbpedia.org/resource/") > -1) {
+			if ($current_entity != $triple["s"]["value"]) {
+				$current_entity = $triple["s"]["value"];
+				
+				if (array_key_exists("i", $triple))
+					array_push($data["object_properties"]["incoming"], array("s" => $triple["s"], "p" => $triple["p"], "c" => array($triple["c"]), "i" => $triple["i"], "j" => $triple["j"]));
+				else	
+					array_push($data["object_properties"]["incoming"], array("s" => $triple["s"], "p" => $triple["p"]));
+			} else {
+				array_push($data["object_properties"]["incoming"][count($data["object_properties"]["incoming"])-1]["c"], $triple["c"]);
+			}
+		}
 		# data_properties
 		elseif ($triple["p"]["value"] == "http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
 			array_push($data["types"], $triple["o"]);
