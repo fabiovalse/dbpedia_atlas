85f52e92539e7548d3a1a3098fb0e9b521ebf75e
fabiovalse <fabio.valsecchi@iit.cnr.it>
2015-03-11 12:24:52 +0100

    Lookup service in search box

--- api/lookup.php
+++ api/lookup.php
@@ -1,6 +1,8 @@
 <?php
 	header('content-type: application/json; charset=utf-8');
 
+	include "jena.php";
+	
 	$hits = 10;
 	$class = "";
 
@@ -17,8 +19,10 @@
 		$command = 'curl -H "Accept: application/json" "http://lookup.dbpedia.org/api/search.asmx/KeywordSearch?QueryString=' . $text . '&QueryClass=' . $class . '&MaxHits=' . $hits . '"';
 		$result = json_decode(shell_exec($command));
 
-		foreach ($result->results as $enity) {
-			$data[] = array("uri" => $enity->uri);
+		foreach ($result->results as $entity) {
+			$in_jena = make_query(urlencode("ASK {<" . $entity->uri . "> <http://wafi.iit.cnr.it/lod/ns/atlas#i> ?i . <" . $entity->uri . "> <http://wafi.iit.cnr.it/lod/ns/atlas#j> ?j}"));
+
+			$data[] = array("uri" => $entity->uri, "in_jena" => $in_jena["boolean"]);
 		}
 
 		echo json_encode($data);
--- result_box/result_box.js
+++ result_box/result_box.js
@@ -11,22 +11,21 @@ result_box.update = function(selection) {
 	result_box.node.html("");
 	result_box.node.style("display", "inline");
 
-	/*html = "";
-	selection.forEach(function(d) { html += "<div class='entity_result' title='" + d.uri + "'>" + format_uri(d.uri) + "</div>";});
-	result_box.node.html(html);*/
-
 	var results = result_box.node.selectAll('.entity_result')
 		.data(selection);
 
 	results.enter().append('div')
-		.attr('class', 'entity_result')
+		.attr('class', function(d) {return (d.in_jena) ? 'entity_result' : 'entity_result disabled';})
 		.attr('id', function(d) {return d.uri;})
+		.attr('title', function(d) {return (d.in_jena) ? d.uri : d.uri + ' currently out of map.';})
 		.text(function(d) {return format_uri(d.uri);})
-		.on('click', function() {
-			trigger(result_box.node, 'select', {uri: this.id});
+		.on('click', function(d) {
+			if (d.in_jena) {
+				trigger(result_box.node, 'select', {uri: this.id});
 
-			result_box.node.style("display", "none");
-			d3.select('#search_textbox').text("");
+				result_box.node.style("display", "none");
+				d3.select('#search_textbox').text("");
+			}
 		});
 }
 
